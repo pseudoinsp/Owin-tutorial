ef53fc872672137cdcd54e0cae1d4431fce89db5
eberki <gonosztaviszorny@gmail.com>
2019-01-08 21:06:55 +0100

    Add project files.

---
+++ OWIN/App_Code/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Startup
+{
+    public static void Configuration(IAppBuilder app)
+    {
+        app.Use(async (ctx, next) =>
+        {
+            Debug.WriteLine("Incoming request: " + ctx.Request.Path);
+
+            await next();
+
+            Debug.WriteLine("Outgpoing response: " + ctx.Request.Path);
+        });
+
+        app.Use(async (ctx, next) =>
+        {
+            await ctx.Response.WriteAsync("<html><body>Hello world</body></html>");
+        });
+    }
+}
